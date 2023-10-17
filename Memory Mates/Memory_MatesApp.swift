3f8c617d74565835e8f1583e4dd8dd3d59faa948
laylalayla100 <awesomeljh@icloud.com>
2023-10-16 22:04:46 -0400

    Initial Commit

---
+++ Memory Mates/Memory_MatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_MatesApp.swift
+//  Memory Mates
+//
+//  Created by layla hunter on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Memory_MatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
